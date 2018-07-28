00369e0cadcfb5fbc80bac37b60092218d7f5ea5
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-07-28 16:20:06 +0200

    build auth module and set external json file

---
+++ middleware/auth.js
@@ -0,0 +1,115 @@
+// from db calls (need promise result to be a json object)
+const permissions = [
+  {
+    action: "no access",
+    value: "0"
+  },
+  {
+    action: "use this funcion",
+    value: "1"
+  },
+  {
+    action: "use a second function",
+    value: "2"
+  },
+  {
+    action: "use another function",
+    value: "3"
+  },
+  {
+    action: "last function to use",
+    value: "4"
+  }
+];
+const users = [
+  {
+    name: "admin",
+    id: "1"
+  },
+  {
+    name: "pasquale",
+    id: "2"
+  },
+  {
+    name: "rajo",
+    id: "3"
+  },
+  {
+    name: "guest",
+    id: "4"
+  }
+];
+
+    const user_permission = require("../middleware/user_permission.json");
+
+    class UserAuth {
+      constructor(keys) {
+        this.keys = keys;
+      }
+
+      // returning true if user have permission
+      can(id, perm) {
+        let auth = false;
+        if (!id || !perm) {
+          console.log("error");
+        } else {
+          this.keys.forEach(key => {
+            if (key.userId == id && key.permissionId == perm) {
+              auth = true;
+            }
+          });
+        }
+        return auth;
+      }
+
+      // return permission array
+      allow(id, perm) {
+        const data = { userId: id, permissionId: perm };
+        if (!id || !perm) {
+          console.log("no input");
+        } else {
+          this.keys.forEach(key => {
+            if (id !== key.userId || perm === key.permissionId) {
+              console.log("id non esiste oppure permesso esiste");
+            } else {
+              this.keys.push(data);
+            }
+          });
+        }
+        return (this.keys = this.removeDuplicates(this.keys));
+      }
+
+      // return permission array
+      disallow(id, perm) {
+        if (!id || !perm) {
+          console.log("error");
+        } else {
+          const result = this.keys.findIndex(data => {
+            return data.userId === id && data.permissionId === perm;
+          });
+          this.keys.splice(result, 1);
+        }
+        return (this.keys = this.removeDuplicates(this.keys));
+      }
+
+      removeDuplicates(arr) {
+        const userIds = [];
+        const result = arr.filter((element, index, array) => {
+          var tempArr = element.userId + "" + element.permissionId;
+          if (userIds.indexOf(tempArr) === -1) {
+            userIds.push(tempArr);
+            return element;
+          }
+        });
+        result.sort((a, b) => {
+          return a.userId - b.userId;
+        });
+        return result
+      }
+    }
+
+    // const auth = new UserAuth(user_permission);
+
+    module.exports.UserAuth = UserAuth;
+    // module.exports.auth = auth;
+
--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -1,4 +1,4 @@
-const can = require('./index').can;
+const AuthModuke = require('./auth').UserAuth;
 
 test('userId = 1 and permissionId = 1 should be true', () => {
     expect(can(1, 1)).toBe(true)
